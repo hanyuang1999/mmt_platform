25fed6b2cb67ccb0fbe935717de2f571b1240384
jerry.han <jerry.han@momenta.ai>
2023-05-12 10:52:10 +0800

    111

--- toker_UI/server_run.sh
+++ toker_UI/server_run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 function cleanup {
     echo "Stopping Python processes..."
-    kill $PID1 $PID2
+    kill $PID1 $PID2 $PID3
 }
 # 当收到退出信号时执行cleanup函数
 trap cleanup EXIT
@@ -11,6 +11,8 @@ python server_folder.py &
 PID1=$!
 python server_socket.py &
 PID2=$!
+python toker_app.py &
+PID3=$!
 
 # 等待所有子进程完成（如果有一个手动停止，其他子进程也会被终止）
-wait $PID1 $PID2
+wait $PID1 $PID2 $PID3
