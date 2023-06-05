6a9864c88b6dc6937cf28145661e3bc06894e976
jerry.han <jerry.han@momenta.ai>
2023-06-05 15:46:48 +0800

    get渲染获取目录

---
+++ start.sh
@@ -0,0 +1,2 @@
+cd /sensorhub_web_toker/web_toker/toker_UI
+python3 toker_app.py
--- tokerApp/views.py
+++ tokerApp/views.py
@@ -15,6 +15,7 @@ import json
 import os
 import time
 import subprocess
+import pytest
 
 # Create your views here.
 # def index(request):
@@ -28,6 +29,38 @@ def login_required_decorator(view_func):
             return redirect(reverse("registration/login.html"))
         return view_func(request, *args, **kwargs)
     return _wrapped_view
+
+class TestCaseCollector:
+    def __init__(self):
+        self.test_cases = []
+
+    def pytest_collection_modifyitems(self, items):
+        for item in items:
+            if isinstance(item, pytest.Function):
+                self.test_cases.append(item.name)
+
+
+def get_testcase_names(test_directory):
+    collector = TestCaseCollector()
+    pytest.main(["--collect-only", "-q", test_directory], plugins=[collector])
+    return collector.test_cases
+
+def get_dir_structure(base_path):
+    dir_structure = {}
+
+    for folder in os.listdir(base_path):
+        folder_path = os.path.join(base_path, folder)
+        
+        if os.path.isdir(folder_path):
+            dir_structure[folder] = {}
+
+            for file in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, file)
+
+                if os.path.isfile(file_path):
+                    dir_structure[folder][file] = get_testcase_names(file_path)
+
+    return dir_structure
     
 
 class My_Server(View):
@@ -45,7 +78,12 @@ class My_Server(View):
         return super().dispatch(*args, **kwargs)
 
     def get(self, request):
-        return render(request, 'web_UI/index.html')
+        base_path = "/sensorhub_web_toker/web_toker/templates/testcase/"
+        dir_structure = get_dir_structure(base_path)
+        context = {
+            'dir_structure':json.dumps(dir_structure)
+        }
+        return render(request, 'web_UI/index.html', context)
     
     def post(self, request):
         print("Get POST requst...")
