b9487c96ceaadcc7a4e938504998f3046f7dc7c9
wiggins.wei <wiggins.wei@momenta.ai>
2023-07-11 16:35:16 +0800

    last dance

--- templates/testcase/socket_tools.py
+++ templates/testcase/socket_tools.py
@@ -3,9 +3,10 @@ import asyncio
 import websockets
 import json
 import time
-
+from django.conf import settings
 async def log_socket(cur_step, remark):
-        async with websockets.connect("ws://10.8.146.24:19799") as ws:
+        current_ip = settings.SERVER_IP
+        async with websockets.connect("ws://"+ current_ip + ":19799") as ws:
             data = {
                 "cur_step": cur_step,
                 "remark": remark
@@ -15,7 +16,8 @@ async def log_socket(cur_step, remark):
             time.sleep(2)
 
 async def result_socket(test_name, result_status, result_remark):
-        async with websockets.connect("ws://10.8.146.24:19799") as ws:
+        current_ip = settings.SERVER_IP
+        async with websockets.connect("ws://"+ current_ip + ":19799") as ws:
             log_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
             data = {
                 "log_time": log_time,
